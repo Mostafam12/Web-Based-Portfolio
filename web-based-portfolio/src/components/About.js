6c3700f721509f81f45f4495689182a928ba5c61
Mostafam12 <86141945+Mostafam12@users.noreply.github.com>
2023-09-29 10:08:43 -0600

    Update Description

--- web-based-portfolio/src/components/About.js
+++ web-based-portfolio/src/components/About.js
@@ -10,18 +10,26 @@ export default function About() {
                         textStyle={{ fontFamily: 'American Typewriter, serif', color: 'white', fontSize: "35px" }}
                         startDelay={100}
                         cursorColor="white"
-                        text="Hi, I'm Mostafa, Welcome to my web based portfolio!"
+                        text="Hi, I'm Mostafa, Welcome to my web-based portfolio!"
                         typeSpeed={100}
                     />
           <p className="mb-8 leading-relaxed">
-            I'm a third-year undergraduate student at the University of Alberta currently taking a Specialization in Computer Science.
+            I'm a fourth-year undergraduate student at the University of Alberta currently enrolled in a Specialization in Computer Science program.
             I'm passionate about computer science and technology, and I'm always looking for ways to expand my knowledge and skills in these fields.
           </p>
 
           <p className="mb-8 leading-relaxed">
-            I have a strong academic background in computer science with a focus on data analysis, management and machine learning.
+            I am currently working as a Research Analyst Intern at the University of Alberta, where I've had the opportunity to closely collaborate with researchers, diving deep into their needs and crafting user-friendly interfaces to enhance their workflows.
+                          My role often sees me meticulously handling data, ensuring its accuracy, consistency, and integrity, which is vital for the success of research projects. 
+                          Beyond the technical tasks, I've taken an active role in knowledge sharing, assisting in drafting documentation, troubleshooting guides, and knowledge base articles. This not only streamlines processes but also empowers researchers to troubleshoot independently. 
+                          Moreover, I provide hands-on technical support, addressing both hardware and software challenges faced by researchers.            
+            
+                          </p>
+
+          <p className="mb-8 leading-relaxed">
+            I have a strong academic background in computer science with a focus on data analysis, management, and machine learning.
             I have completed courses in File and Database Management, Algorithms and Data Structures, Formal Systems 
-            & Mathematical Logic, Visual Recognition, Information Retrieval, Object-oriented design, and 
+            & Mathematical Logic, Visual Recognition, Information Retrieval, Object-oriented design, Software Practices, Natural-Language Processing, Mobile App Development, and  
             Machine Learning & Intelligent Systems.
           </p>
           <div className="flex justify-center">
